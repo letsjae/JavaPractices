e287397bc6c8c633eeecdfb0f2ad2ef4fa4d5032
Jey <jaeld94@gmail.com>
2018-03-31 11:21:38 -0300

    mas cambios

---
+++ DDS HW/src/main/java/DDS/Prenda.java
@@ -0,0 +1,25 @@
+package DDS;
+
+//public class HelloWorld {
+//
+//    public static void main(String[] args) {
+//        // Prints "Hello, World" to the terminal window.
+//    	System.out.println("Hello, uh UTN!");
+//
+//    }
+//
+//}
+
+public class Prenda{
+	private String estado;
+	
+	public void estadoPrenda(String nombre) {
+		  this.estado = nombre;
+		}
+
+	
+	public String precioPrenda(){
+		return this.estado;
+	}
+	
+}
